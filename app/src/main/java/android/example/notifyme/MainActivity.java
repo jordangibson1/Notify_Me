a53b49bffc44e550fdedb29eff414249bcf7f45a
Jordan Gibson <gibsonj22@hanover.edu>
2020-03-16 12:39:42 -0400

    Completed Task 2, added update and cancel buttons

--- app/src/main/java/android/example/notifyme/MainActivity.java
+++ app/src/main/java/android/example/notifyme/MainActivity.java
@@ -7,6 +7,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
     private static final int NOTIFICATION_ID = 0;
+    private Button button_cancel;
+    private Button button_update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +34,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         createNotificationChannel();
+        button_update = findViewById(R.id.update);
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateNotification();
+            }
+        });
+
+        button_cancel = findViewById(R.id.cancel);
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               cancelNotification();
+            }
+        });
+        setNotificationButtonState(true, false, false);
     }
 
     public void sendNotification() {
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
         mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+        setNotificationButtonState(false, true, true);
     }
 
     public void createNotificationChannel()
@@ -69,4 +90,25 @@ public class MainActivity extends AppCompatActivity {
         return notifyBuilder;
     }
 
-}
+    public void updateNotification() {
+        Bitmap androidImage = BitmapFactory
+                .decodeResource(getResources(),R.drawable.mascot_1);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(androidImage)
+                .setBigContentTitle("Notification Updated!"));
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+        setNotificationButtonState(false, false, true);
+    }
+
+    public void cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+        setNotificationButtonState(true, false, false);
+    }
+
+    void setNotificationButtonState(Boolean isNotifyEnabled, Boolean isUpdateEnabled, Boolean isCancelEnabled) {
+            button_notify.setEnabled(isNotifyEnabled);
+            button_update.setEnabled(isUpdateEnabled);
+            button_cancel.setEnabled(isCancelEnabled);
+        }
+    }
